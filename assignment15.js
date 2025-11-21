beb23ab094cb5c382f86b78be1257801caf46dbe
saqib mearani <saqibmearani@gmail.com>
2025-11-21 11:25:19 +0500

    Add files via upload

---
+++ assignment15.js
@@ -0,0 +1,24 @@
+var studentnames = []
+studentnames.push("Ali")
+studentnames.push("Saqib")
+console.log(studentnames)
+
+//
+var studentNames = new Array()
+studentNames.push("abdullah")
+studentNames.push("rayan")
+console.log(studentNames)
+
+
+//
+var fruit = ["apple", "banana", "cherry", "mango"];
+console.log(fruit);
+
+
+//
+var number = [10,20, 30, 40, 50]
+console.log(number)
+
+//
+var mixedarray = ["Saqib", 25, true,  ,3.14 ];
+console.log(mixedarray);
