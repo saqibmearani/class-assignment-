e54b41fa7f979af363e3a41a6e5b8c70b4509ebb
saqib mearani <saqibmearani@gmail.com>
2025-12-12 23:26:49 +0500

    Add files via upload

---
+++ assignment18.js
@@ -0,0 +1,59 @@
+// chap # 1
+alert("Welcome to JavaScript!")
+
+var name =prompt("enter your name")
+alert(name)
+
+var color =prompt("enter your fav color")
+alert(color)
+// chap # 2
+var fname =prompt("fname")
+var lname=prompt("lname")
+alert(fname + lname)
+
+var city ="karachi"
+alert("you live in "+city)
+
+var hobby =prompt("enter your hobby")
+alert(hobby)
+//chap # 3
+var a =4
+var b =22
+var c=a+b
+alert(c)
+
+var mark= prompt("mark")
+if(mark>=50){
+    alert("pass")
+}
+else{
+    alert("fail")
+}
+
+var age= prompt("age")
+if(age>=18){
+    alert("you are above 18")
+}
+else{
+    alert("you are under 18")
+}
+//chap # 4
+var user ="saqib"
+var user_name="abdullah"
+var $price=40
+alert(user+user_name+$price)
+
+123data =122
+user name ="sqa"
+var-name="jjjj"
+alert(123data+user name+var-name)
+
+//chap # 5
+var a =4
+var b =22
+var c=a+b
+alert(c)
+
+
+for(a=1;a<1;a++)    
+alert(a)
